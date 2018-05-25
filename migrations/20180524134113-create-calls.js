270a61758a7a96290e3883b93fe77997db013af2
Pablo Bender <pablo@bender.eti.br>
2018-05-25 16:09:32 -0400

    create agents and add calls columns

--- migrations/20180524134113-create-calls.js
+++ migrations/20180524134113-create-calls.js
@@ -1,23 +1,28 @@
 const {dataType} = require('db-migrate-shared')
-const {STRING, DATE_TIME, INTEGER} = dataType
+const {STRING, DATE_TIME, INTEGER, SMALLINT} = dataType
 const callsTable = 'calls'
 
 exports.up = async (db) => {
   await db.createTable(callsTable, {
     id: {type: STRING, length: 128, notNull: true, primaryKey: true},
     created_at: {type: DATE_TIME, notNull: true},
+    answer_at: {type: DATE_TIME},
+    agent_offers: {type: SMALLINT, notNull: true, defaultValue: 0},
     caller_number: {type: STRING, length: 30, notNull: true},
     caller_info: {type: STRING, length: 30},
-    position: {type: INTEGER, notNull: true},
+    hold_secs: {type: SMALLINT, notNull: true, defaultValue: 0},
+    talk_secs: {type: SMALLINT, notNull: true, defaultValue: 0},
+    ring_secs: {type: SMALLINT, notNull: true, defaultValue: 0},
     initial_position: {type: INTEGER, notNull: true},
+    transfered_to: {type: INTEGER}
   })
   await db.runSql(`ALTER TABLE ${callsTable} ADD COLUMN direction ENUM('IN', 'OUT', 'AUTO') NOT NULL`)
-  await db.runSql(`ALTER TABLE ${callsTable} ADD COLUMN status ENUM('ENQUEUED') NOT NULL`)
+  await db.runSql(`ALTER TABLE ${callsTable} ADD COLUMN status ENUM('ENQUEUED', 'CONNECTED') NOT NULL`)
   await db.addIndex(callsTable, 'index_calls_caller_number', ['caller_number'])
   await db.addIndex(callsTable, 'index_calls_created_at', ['created_at'])
   await db.addIndex(callsTable, 'index_calls_status', ['status'])
 }
 
 exports.down = (db) => (
-   db.dropTable(callsTable)
+  db.dropTable(callsTable)
 )
---
+++ migrations/20180525160950-create-agents.js
@@ -0,0 +1,23 @@
+const {dataType} = require('db-migrate-shared')
+const {INTEGER} = dataType
+
+exports.up = async (db) => {
+  await db.createTable('agents', {
+    id: {type: INTEGER, notNull: true, primaryKey: true, autoIncrement: true}
+  })
+  await db.addColumn('calls', 'agent_id', {type: INTEGER})
+  await db.addForeignKey('calls', 'agents', 'fk_calls_agents', { agent_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
+  await db.addIndex('calls', 'index_calls_agent_id', ['agent_id'])
+
+  await db.addColumn('call_history', 'agent_id', {type: INTEGER})
+}
+
+exports.down = async (db) => {
+  await db.removeForeignKey('calls', 'fk_calls_agents')
+  await db.removeIndex('calls', 'index_calls_agent_id')
+  await db.removeColumn('calls', 'agent_id')
+
+  await db.removeColumn('call_history', 'agent_id')
+
+  await db.dropTable('agents')
+}
