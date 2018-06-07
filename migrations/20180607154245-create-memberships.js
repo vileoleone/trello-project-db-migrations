5a6129bfb60410443b7ca6a4f6a11092538d1fd5
Pablo Bender <pablo@bender.eti.br>
2018-06-07 15:22:36 -0400

    create memberships table

---
+++ migrations/20180607154245-create-memberships.js
@@ -0,0 +1,17 @@
+const {dataType} = require('db-migrate-shared')
+const {SMALLINT, INTEGER, BIGINT, STRING} = dataType
+
+exports.up = async (db) => {
+  await db.createTable('memberships', {
+    queue_id: {type: STRING, notNull: true, length: 128},
+    agent_id: {type: INTEGER, notNull: true},
+    penalty: {type: SMALLINT, notNull: true, defaultValue: 0}
+  })
+  await db.runSql('ALTER TABLE memberships ADD CONSTRAINT pk_memberships PRIMARY KEY (agent_id, queue_id)')
+  await db.addForeignKey('memberships', 'agents', 'fk_memberships_agents', { agent_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
+  await db.addForeignKey('memberships', 'queues', 'fk_memberships_queues', { queue_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
+}
+
+exports.down = async (db) => {
+  await db.dropTable('memberships')
+}
