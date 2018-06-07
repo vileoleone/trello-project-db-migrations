d5518156ddf0247b743c0db754fd39980bd575eb
Lucas de Souza Azambuja <lucassouzaazambuja2@gmail.com>
2018-06-07 12:16:13 -0400

    QUEUEMEMBER_PAUSE add status paused

--- migrations/20180525160950-create-agents.js
+++ migrations/20180525160950-create-agents.js
@@ -1,5 +1,5 @@
 const {dataType} = require('db-migrate-shared')
-const {SMALLINT, INTEGER, STRING, DATE_TIME} = dataType
+const {INTEGER, STRING, DATE_TIME} = dataType
 
 exports.up = async (db) => {
   await db.createTable('agents', {
@@ -11,10 +11,10 @@ exports.up = async (db) => {
     talking_call_id: {type: STRING, length: 128},
     last_call_rejected_at: {type: DATE_TIME},
     last_call_answered_at: {type: DATE_TIME},
-    talking_since: {type: DATE_TIME},
-    pause_reason: {type: SMALLINT}
+    talking_since: {type: DATE_TIME}
   })
-  await db.runSql(`ALTER TABLE agents ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'ONTHEPHONE') NOT NULL DEFAULT 'OFFLINE'`)
+  await db.runSql(`ALTER TABLE agents ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'ONTHEPHONE', 'PAUSED') NOT NULL DEFAULT 'OFFLINE'`)
+  await db.runSql(`ALTER TABLE agents ADD COLUMN pause_reason_id TINYINT`)
   await db.addColumn('calls', 'agent_id', {type: INTEGER})
   await db.addForeignKey('calls', 'agents', 'fk_calls_agents', { agent_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
   await db.addIndex('calls', 'index_calls_agent_id', ['agent_id'])
