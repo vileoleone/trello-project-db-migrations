a2786f7d751a812197a962936d09832cf62838ba
Lucas de Souza Azambuja <lucassouzaazambuja2@gmail.com>
2018-06-07 11:51:46 -0400

    QUEUEMEMBER_PAUSE add fields in tables

--- migrations/20180525160950-create-agents.js
+++ migrations/20180525160950-create-agents.js
@@ -1,16 +1,18 @@
 const {dataType} = require('db-migrate-shared')
-const {INTEGER, STRING, DATE_TIME} = dataType
+const {SMALLINT, INTEGER, STRING, DATE_TIME} = dataType
 
 exports.up = async (db) => {
   await db.createTable('agents', {
     id: {type: INTEGER, notNull: true, primaryKey: true, autoIncrement: true},
     login_extension: {type: STRING, length: 6},
     login_at: {type: DATE_TIME},
+    pause_at: {type: DATE_TIME},
     logout_at: {type: DATE_TIME},
     talking_call_id: {type: STRING, length: 128},
     last_call_rejected_at: {type: DATE_TIME},
     last_call_answered_at: {type: DATE_TIME},
-    talking_since: {type: DATE_TIME}
+    talking_since: {type: DATE_TIME},
+    pause_reason: {type: SMALLINT}
   })
   await db.runSql(`ALTER TABLE agents ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'ONTHEPHONE') NOT NULL DEFAULT 'OFFLINE'`)
   await db.addColumn('calls', 'agent_id', {type: INTEGER})
