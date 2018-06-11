07afa070771142b7ee90727acc3730912031ad63
Pablo Bender <pablo@bender.eti.br>
2018-06-11 17:25:02 -0400

    add transfer counters to queue_summary

--- migrations/20180528221621-create-queue-summary.js
+++ migrations/20180528221621-create-queue-summary.js
@@ -6,20 +6,33 @@ exports.up = async (db) => {
     queue_id: {type: STRING, notNull: true, length: 128},
     period: {type: DATE_TIME, notNull: true},
     in_completed: {type: INTEGER, notNull: true, defaultValue: 0},
+    in_transferred: {type: INTEGER, notNull: true, defaultValue: 0},
+    in_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
+    in_completed_sla: {type: INTEGER, notNull: true, defaultValue: 0},
+    in_abandoned_sla: {type: INTEGER, notNull: true, defaultValue: 0},
+
     out_completed: {type: INTEGER, notNull: true, defaultValue: 0},
+    out_transferred: {type: INTEGER, notNull: true, defaultValue: 0},
+    out_discarded: {type: INTEGER, notNull: true, defaultValue: 0},
+
     auto_completed: {type: INTEGER, notNull: true, defaultValue: 0},
+    auto_transferred: {type: INTEGER, notNull: true, defaultValue: 0},
+    auto_discarded: {type: INTEGER, notNull: true, defaultValue: 0},
+    auto_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
+
     in_call_secs: {type: INTEGER, notNull: true, defaultValue: 0},
     out_call_secs: {type: INTEGER, notNull: true, defaultValue: 0},
     auto_call_secs: {type: INTEGER, notNull: true, defaultValue: 0},
-    in_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
-    auto_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
+
     in_hold_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
-    out_try_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
-    auto_hold_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
     in_hold_secs_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
+
+    out_try_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
+    out_try_secs_discarded: {type: INTEGER, notNull: true, defaultValue: 0},
+
+    auto_hold_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
     auto_hold_secs_abandoned: {type: INTEGER, notNull: true, defaultValue: 0},
-    auto_try_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0},
-    out_try_secs_discarded: {type: INTEGER, notNull: true, defaultValue: 0}
+    auto_try_secs_completed: {type: INTEGER, notNull: true, defaultValue: 0}
   })
   await db.runSql('ALTER TABLE queue_summary ADD CONSTRAINT pk_queue_summary PRIMARY KEY (queue_id, period)')
   await db.addForeignKey('queue_summary', 'queues', 'fk_queue_summary_queues', { queue_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
