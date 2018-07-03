818b5fc377cb69d2a5f72d0cd191ba885b010b20
Pablo Bender <pablo@bender.eti.br>
2018-07-03 17:31:24 -0400

    add status to memberships

--- migrations/20180607154245-create-memberships.js
+++ migrations/20180607154245-create-memberships.js
@@ -1,5 +1,5 @@
 const {dataType} = require('db-migrate-shared')
-const {SMALLINT, INTEGER, BIGINT, STRING} = dataType
+const {SMALLINT, INTEGER, STRING} = dataType
 
 exports.up = async (db) => {
   await db.createTable('memberships', {
@@ -7,6 +7,7 @@ exports.up = async (db) => {
     agent_id: {type: INTEGER, notNull: true},
     penalty: {type: SMALLINT, notNull: true, defaultValue: 0}
   })
+  await db.runSql(`ALTER TABLE memberships ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'ONTHEPHONE', 'PAUSED') NOT NULL DEFAULT 'OFFLINE'`)
   await db.runSql('ALTER TABLE memberships ADD CONSTRAINT pk_memberships PRIMARY KEY (agent_id, queue_id)')
   await db.addForeignKey('memberships', 'agents', 'fk_memberships_agents', { agent_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
   await db.addForeignKey('memberships', 'queues', 'fk_memberships_queues', { queue_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
