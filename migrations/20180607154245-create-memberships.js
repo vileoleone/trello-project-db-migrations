a05f41f4ac1808e91c050624c390772d741b64c0
Pablo Bender <pablo@bender.eti.br>
2018-07-20 11:19:18 -0400

    add agent DIALING status to memberships

--- migrations/20180607154245-create-memberships.js
+++ migrations/20180607154245-create-memberships.js
@@ -7,7 +7,7 @@ exports.up = async (db) => {
     agent_id: {type: INTEGER, notNull: true},
     penalty: {type: SMALLINT, notNull: true, defaultValue: 0}
   })
-  await db.runSql(`ALTER TABLE memberships ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'ONTHEPHONE', 'PAUSED') NOT NULL DEFAULT 'OFFLINE'`)
+  await db.runSql(`ALTER TABLE memberships ADD COLUMN status ENUM('OFFLINE', 'ONLINE', 'RINGING', 'DIALING', 'ONTHEPHONE', 'PAUSED') NOT NULL DEFAULT 'OFFLINE'`)
   await db.runSql('ALTER TABLE memberships ADD CONSTRAINT pk_memberships PRIMARY KEY (agent_id, queue_id)')
   await db.addForeignKey('memberships', 'agents', 'fk_memberships_agents', { agent_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
   await db.addForeignKey('memberships', 'queues', 'fk_memberships_queues', { queue_id: 'id' }, {onDelete: 'RESTRICT', onUpdate: 'RESTRICT'})
