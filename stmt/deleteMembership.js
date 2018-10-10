0091e99d107e018343e794d4de7d15a39cfeb0e9
Pablo Bender <pablo@bender.eti.br>
2018-10-10 18:29:19 -0400

    add missing edited stmt/deleteMembership.js

--- stmt/deleteMembership.js
+++ stmt/deleteMembership.js
@@ -1,9 +1,18 @@
 import execute from '../lib/execute'
 
-export default async (db, dbName, agentId) => {
-  const sql = `
-  DELETE FROM ${dbName}.memberships
-  WHERE agent_id = ?
-  `
+export default async (db, dbName, agentId, queueId = null) => {
+  if (queueId === null) {
+    return deleteByAgent(db, dbName, agentId)
+  }
+  return deleteByAgentQueue(db, dbName, agentId, queueId)
+}
+
+const deleteByAgent = async (db, dbName, agentId) => {
+  const sql = `DELETE FROM ${dbName}.memberships WHERE agent_id = ?`
   return execute(db, sql, [agentId])
+}
+
+const deleteByAgentQueue = async (db, dbName, agentId, queueId) => {
+  const sql = `DELETE FROM ${dbName}.memberships WHERE agent_id = ? and queue_id = ?`
+  return execute(db, sql, [agentId, queueId])
 }
