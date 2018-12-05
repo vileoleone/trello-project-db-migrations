6427ce8dcf9177561a9e5d6d5858db04bf15664a
Pablo Bender <pablo@bender.eti.br>
2018-12-05 15:42:34 -0300

    single queue_properties creation

--- migrations/20181122164630-create-queue-properties.js
+++ migrations/20181122164630-create-queue-properties.js
@@ -6,13 +6,13 @@ const queueIdType = { type: STRING, length: 128 }
 exports.up = async (db) => {
   await db.createTable('queue_properties', {
     id: { type: BIGINT, notNull: true, primaryKey: true, autoIncrement: true },
-    queue_id: { ...queueIdType, notNull: true },
+    queue_id: { ...queueIdType, notNull: false },
     name: { type: STRING, length: 128, notNull: true },
     value: { type: TEXT }
   })
   await db.addForeignKey('queue_properties', 'queues', 'fk_queue_properties_queues', { queue_id: 'id' }, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
   await db.addIndex('queue_properties', 'index_queue_properties_queue_id', ['queue_id'])
-  await db.addIndex('queue_properties', 'unique_queue_properties_queue_id_name', ['queue_id', 'name'])
+  await db.addIndex('queue_properties', 'unique_queue_properties_queue_id_name', ['queue_id', 'name'], true)
 }
 
 exports.down = async (db) => {
