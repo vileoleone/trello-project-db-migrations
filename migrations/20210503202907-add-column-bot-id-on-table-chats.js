e4187569f8f47488e1fcf8535b346b336ae550cd
Talles Enrico <enrico.talles@gmail.com>
2021-05-03 17:02:48 -0400

    fix: alter fk name on bot_id

--- migrations/20210503202907-add-column-bot-id-on-table-chats.js
+++ migrations/20210503202907-add-column-bot-id-on-table-chats.js
@@ -3,10 +3,10 @@ const { STRING } = dataType
 
 exports.up = async (db) => {
   await db.addColumn('chats', 'bot_id', { type: STRING, length: 128, notNull: true })
-  await db.addForeignKey('chats', 'applications', 'fk_bot_id', { bot_id: 'id' }, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
+  await db.addForeignKey('chats', 'applications', 'fk_bot_application_id', { bot_id: 'id' }, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
 }
 
 exports.down = async (db) => {
-  await db.removeForeignKey('chats', 'fk_bot_id')
+  await db.removeForeignKey('chats', 'fk_bot_application_id')
   await db.removeColumn('chats', 'bot_id')
 }
