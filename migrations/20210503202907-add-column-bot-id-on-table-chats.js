81cc2d8a8afddc38a996c51561cf1f0fb9fde52e
Talles Enrico <enrico.talles@gmail.com>
2021-05-03 16:42:00 -0400

    feat: add column bot_id on table chats

---
+++ migrations/20210503202907-add-column-bot-id-on-table-chats.js
@@ -0,0 +1,12 @@
+const { dataType } = require('db-migrate-shared')
+const { STRING } = dataType
+
+exports.up = async (db) => {
+  await db.addColumn('chats', 'bot_id', { type: STRING, length: 128, notNull: true })
+  await db.addForeignKey('chats', 'applications', 'fk_bot_id', { bot_id: 'id' }, { onDelete: 'RESTRICT', onUpdate: 'RESTRICT' })
+}
+
+exports.down = async (db) => {
+  await db.removeForeignKey('chats', 'fk_bot_id')
+  await db.removeColumn('chats', 'bot_id')
+}
