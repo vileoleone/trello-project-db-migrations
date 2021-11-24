32d8ec9d3644c4e224dbb46061f2cb42c8e838d8
Talles Enrico <enrico.talles@gmail.com>
2021-11-24 11:24:29 -0400

    fix: adjust column welcome_message on bots

--- migrations/20211124144405-add-column-welcome-message-on-bots.js
+++ migrations/20211124144405-add-column-welcome-message-on-bots.js
@@ -2,9 +2,9 @@ const { dataType } = require('db-migrate-shared')
 const { TEXT } = dataType
 
 exports.up = async (db) => {
-  await db.addColumn('bots', 'welcome-message', { type: TEXT })
+  await db.addColumn('bots', 'welcome_message', { type: TEXT })
 }
 
 exports.down = async (db) => {
-  await db.removeColumn('bots', 'welcome-message')
+  await db.removeColumn('bots', 'welcome_message')
 }
