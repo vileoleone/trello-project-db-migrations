5b92b14b700b40fe34ff86bcae737cade32e577d
Talles Enrico <enrico.talles@gmail.com>
2021-12-28 12:06:15 -0400

    feat: create table bot_templates

--- migrations/20211222130631-create-table-templates.js
+++ migrations/20211222130631-create-table-templates.js
@@ -1,15 +1,16 @@
 const { dataType } = require('db-migrate-shared')
-const { CHAR, BIGINT, INTEGER, DATE_TIME, STRING, TEXT } = dataType
+const { BIGINT, DATE_TIME, STRING, TEXT } = dataType
 
 exports.up = async (db) => {
   await db.createTable('bot_templates', {
-    id: { type: INTEGER, length: 11, notNull: true, primaryKey: true, autoIncrement: true },
+    id: { type: BIGINT, notNull: true, primaryKey: true, autoIncrement: true },
+    active: { type: 'TINYINT', length: 1, notNull: true, defaultValue: 1 },
     bot_id: { type: BIGINT, notNull: true },
     name: { type: STRING, length: 256, notNull: true },
-    template_id: { type: STRING, length: 256 },
-    status: { type: CHAR, length: 36 },
-    category: { type: 'ENUM', length: "'ACCOUNT_UPDATE', 'PAYMENT_UPDATE', 'PERSONAL_FINANCE_UPDATE', 'SHIPPING_UPDATE', 'RESERVATION_UPDATE', 'ISSUE_RESOLUTION', 'APPOINTMENT_UPDATE', 'TRANSPORTATION_UPDATE', 'TICKET_UPDATE', 'ALERT_UPDATE', 'AUTO_REPLY'" },
     message: { type: TEXT, notNull: true },
+    status: { type: STRING, length: 36 },
+    template_id: { type: STRING, length: 36 },
+    category: { type: 'ENUM', notNull: true, length: "'ACCOUNT_UPDATE', 'PAYMENT_UPDATE', 'PERSONAL_FINANCE_UPDATE', 'SHIPPING_UPDATE', 'RESERVATION_UPDATE', 'ISSUE_RESOLUTION', 'APPOINTMENT_UPDATE', 'TRANSPORTATION_UPDATE', 'TICKET_UPDATE', 'ALERT_UPDATE', 'AUTO_REPLY'" },
     created_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' },
     updated_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' },
     deleted_at: { type: DATE_TIME }
--- migrations/20211228142737-add-column-active-on-bot-templates.js
+++
@@ -1,7 +0,0 @@
-exports.up = async (db) => {
-  await db.addColumn('bot_templates', 'active', { type: 'TINYINT', length: 1, notNull: true, defaultValue: 1 })
-}
-
-exports.down = async (db) => {
-  await db.removeColumn('bot_templates', 'active')
-}
