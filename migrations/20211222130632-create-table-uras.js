63f0bc070399948b905e496216d2b76285b9cd71
Luiz Amorim <luiz.simples@gmail.com>
2021-12-28 14:31:13 -0400

    fix migration create table URAs

--- migrations/20211222130632-create-table-uras.js
+++ migrations/20211222130632-create-table-uras.js
@@ -6,7 +6,7 @@ exports.up = (db) => (
     id: { type: BIGINT, notNull: true, primaryKey: true, autoIncrement: true },
     active: { type: 'TINYINT', length: 1, notNull: true, defaultValue: 1 },
     description: { type: STRING, length: 256 },
-    config: { type: TEXT, defaultValue: '{}' },
+    config: { type: TEXT },
     created_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' },
     updated_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' }
   })
