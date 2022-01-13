55a3889f40989d13ba3b00516b15c84b2a7f4235
Talles Enrico <enrico.talles@gmail.com>
2022-01-13 11:38:42 -0400

    fix: adjust alter name on column chats

--- migrations/20220113122557-alter-name-on-chats.js
+++ migrations/20220113122557-alter-name-on-chats.js
@@ -2,7 +2,7 @@ exports.up = async (db) => {
   const queryResult = await db.runSql("SELECT id, source_id, info FROM chats where source in ('ZENVIA', 'GUPSHUP', 'TWILIO', '360DIALOG')")
   const chats = queryResult.map(chats => ({ ...chats }))
   for (const chat of chats) {
-    const info = chat.info ? JSON.parse(chat.info) : { firstName: '', lastName: '' }
+    const info = jsonParse(chat.info)
     let name = `${info.firstName} ${info.lastName || ''}`.trim()
     name = name ? `${name} - ${chat.source_id}` : chat.source_id
     await updateChat(db, name, chat.id)
@@ -13,7 +13,7 @@ exports.down = async (db) => {
   const queryResult = await db.runSql("SELECT id, info FROM chats where source in ('ZENVIA', 'GUPSHUP', 'TWILIO', '360DIALOG')")
   const chats = queryResult.map(chats => ({ ...chats }))
   for (const chat of chats) {
-    const info = chat.info ? JSON.parse(chat.info) : { firstName: '', lastName: '' }
+    const info = jsonParse(chat.info)
     const name = `${info.firstName} ${info.lastName || ''}`.trim()
     await updateChat(db, name, chat.id)
   }
@@ -22,3 +22,13 @@ exports.down = async (db) => {
 const updateChat = async (db, name, id) => (
   await db.runSql('UPDATE chats SET name = ? WHERE id = ?', [name, id])
 )
+
+const jsonParse = (raw) => {
+  try {
+    return isEmpty(raw) ? { firstName: '', lastName: '' } : JSON.parse(raw)
+  } catch (_) {
+    return { firstName: '', lastName: '' }
+  }
+}
+
+const isEmpty = (val) => [null, undefined, ''].includes(val)
