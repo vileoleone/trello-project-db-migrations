e02e68245119a7e506290676c6d37aa56cc742f7
Talles Enrico <enrico.talles@gmail.com>
2022-01-25 13:21:50 -0400

    fix: set correct times of chat

---
+++ migrations/20220125172125-set-times-chat.js
@@ -0,0 +1,15 @@
+exports.up = async (db) => {
+  await db.runSql('update chats set chat_secs = (unix_timestamp(finished_at) - unix_timestamp(created_at))')
+
+  await db.runSql(`update chats as chat set 
+  hold_secs = (
+    COALESCE((select unix_timestamp(history.created_at) from chat_history as history where chat.id = history.chat_id and history.event = "ACCEPTED" order by id limit 1), 0) +
+    COALESCE((select unix_timestamp(history.created_at) from chat_history as history where chat.id = history.chat_id and history.event = "START" order by id limit 1), 0)
+  )`)
+
+  await db.runSql('update chats set talk_secs = (COALESCE(chat_secs, 0) - COALESCE(hold_secs,0))')
+}
+
+exports.down = async (db) => {
+  await db.runSql('update chats set chat_secs = 0, chathold_secs = 0, chat_secs = 0')
+}
