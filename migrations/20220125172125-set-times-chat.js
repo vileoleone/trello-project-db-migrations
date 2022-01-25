3f749c49a745bbbe6dd274f8bcab5678c6a10496
Talles Enrico <enrico.talles@gmail.com>
2022-01-25 14:06:20 -0400

    fix: set times on chat

--- migrations/20220125172125-set-times-chat.js
+++ migrations/20220125172125-set-times-chat.js
@@ -1,13 +1,7 @@
 exports.up = async (db) => {
-  await db.runSql('update chats set chat_secs = (unix_timestamp(finished_at) - unix_timestamp(created_at))')
-
-  await db.runSql(`update chats as chat set 
-  hold_secs = (
-    COALESCE((select unix_timestamp(history.created_at) from chat_history as history where chat.id = history.chat_id and history.event = "ACCEPTED" order by id limit 1), 0) +
-    COALESCE((select unix_timestamp(history.created_at) from chat_history as history where chat.id = history.chat_id and history.event = "START" order by id limit 1), 0)
-  )`)
-
-  await db.runSql('update chats set talk_secs = (COALESCE(chat_secs, 0) - COALESCE(hold_secs,0))')
+  await db.runSql('update chats set chat_secs = (unix_timestamp(finished_at) - unix_timestamp(created_at)) where finished_at IS NOT null')
+  await db.runSql('update chats set talk_secs = (unix_timestamp(finished_at) - unix_timestamp(answered_at)) where finished_at IS NOT null')
+  await db.runSql('update chats set hold_secs = (unix_timestamp(answered_at) - unix_timestamp(created_at)) where answered_at IS NOT null')
 }
 
 exports.down = async (db) => {
