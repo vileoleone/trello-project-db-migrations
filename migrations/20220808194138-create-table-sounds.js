1da103206237a17773ddfeb0b8441ba31a0d1997
Nathalia Lanzarine <nath_lanzarine@hotmail.com>
2022-08-09 15:31:47 -0400

    add sounds table

---
+++ migrations/20220808194138-create-table-sounds.js
@@ -0,0 +1,17 @@
+const { dataType } = require('db-migrate-shared')
+const { DATE_TIME, STRING, BIGINT } = dataType
+
+exports.up = async (db) => {
+  await db.createTable('sounds', {
+    id: { type: BIGINT, notNull: false, primaryKey: true, autoIncrement: true },
+    description: { type: STRING, length: 256, notNull: true },
+    active: { type: 'TINYINT', length: 1, notNull: true, defaultValue: 1 },
+    created_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' },
+    updated_at: { type: DATE_TIME, notNull: true, defaultValue: 'CURRENT_TIMESTAMP' },
+    sound: { type: STRING, length: 256, notNull: true }
+  })
+}
+
+exports.down = async (db) => (
+  await db.dropTable('sounds')
+)
