029d1424e9f5b503f5757b29eea87b5c04081029
Luiz Amorim <luiz.simples@gmail.com>
2022-11-23 09:26:13 -0400

    change default queue

--- migrations/20220323215648-insert-queue-filaexemplo.js
+++ migrations/20220323215648-insert-queue-filaexemplo.js
@@ -1,11 +1,10 @@
 exports.up = async (db) => {
   const queryResult = await db.runSql('SELECT id FROM queues')
   const queues = queryResult.map(queue => ({ ...queue }))
-  const hasQueue = queues.filter(queue => queue.id === 'filaexemplo')
-
-  if (!hasQueue.length) await db.runSql('INSERT INTO queues (id, name, direction_in, direction_out, direction_auto, description, sla_secs, sla_percent, image_url, active) VALUES ("filaexemplo", "filaexemplo", 1, 1, 0, "filaexemplo", 15, 90, NULL, 1)')
+  const hasQueue = queues.filter(queue => queue.id === 'manual').length
+  if (!hasQueue) await db.runSql('INSERT INTO queues (id, name, direction_in, direction_out, direction_auto, description, sla_secs, sla_percent, image_url, active) VALUES ("manual", "Manual", 1, 1, 0, "Fila Manual", 15, 90, NULL, 1)')
 }
 
 exports.down = async (db) => (
-  await db.runSql("DELETE FROM queues WHERE id = 'filaexemplo'")
+  db.runSql("DELETE FROM queues WHERE id = 'manual'")
 )
